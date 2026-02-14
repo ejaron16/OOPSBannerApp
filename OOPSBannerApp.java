05211d14887dd002d72f85b26643cde627bf6bea
ejaron16 <ee8691@srmist.edu.in>
2026-02-14 20:12:03 +0530

    feature/UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * Goal: Print "OOPS" to the console as a starting point.
+ * * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    // The Main Method is the entry point of the application
+    public static void main(String[] args) {
+        // System.out.println() invokes a method to display a String Literal
+        System.out.println("OOPS");
+    }
+}

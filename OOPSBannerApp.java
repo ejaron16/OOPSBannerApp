1b41d3565b9b081ef5a86d79acea6f5029c0bd5a
ejaron16 <ee8691@srmist.edu.in>
2026-02-16 13:32:07 +0530

    Added OOPSBannerApp

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,20 @@
+/**
+ * @author [Your Name]
+ * @version 1.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        // Displaying OOPS in a Banner Format using * and spaces
-        System.out.println(" **** **** **** **** ");
-        System.out.println("* * * * * * * ");
-        System.out.println("* * * * * * **** ");
-        System.out.println("* * * * * * *");
-        System.out.println(" **** **** **** **** ");
+        // Now we just call the method instead of writing all the print lines here
+        displayOopsBanner();
+    }
+
+    // This is the new function (method) we created to hold the banner logic
+    public static void displayOopsBanner() {
+        System.out.println(" ****   ****  ****  **** ");
+        System.out.println("*    * *    * *   * * ");
+        System.out.println("*    * *    * ****  **** ");
+        System.out.println("*    * *    * *        *");
+        System.out.println(" ****   ****  *     **** ");
     }
 }

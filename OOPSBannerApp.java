c79c6f1689259c745bfb0d24c4cdb2f1effec406
ejaron16 <ee8691@srmist.edu.in>
2026-02-23 22:09:27 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,7 +2,7 @@
  * @author [E JARON]
  * @version 1.0 UC3 - OOPS Banner Using String Join
  */
-public class OOPSSTRJoin {
+public class OOPSBannerApp {
 
     public static void main(String[] args) {
         // Now we just call the method instead of writing all the print lines here
